eeb71420e3d8c8f6a7890da636f91205db90d205
José Cabeda <1130395@isep.ipp.pt>
2017-09-09 13:07:42 +0100

    Moved main page to a react component

--- globart/src/App.js
+++ globart/src/App.js
@@ -1,13 +1,15 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.scss';
-import NavBar from './Components/Global/navBar'
+import NavBar from './Components/Global/navBar';
+import Main from './Components/Main/main';
 
 class App extends Component {
     render() {
         return (
             <div>
                 <NavBar></NavBar>
+                <Main></Main>
             </div>
         );
     }
