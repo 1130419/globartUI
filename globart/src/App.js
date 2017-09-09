4d9d7160020a6a6b65472997cfd30e159dc65482
luis.lopes <ll61295@gmail.com>
2017-09-09 12:20:27 +0100

    Addition of Navbar and Components

--- globart/src/App.js
+++ globart/src/App.js
@@ -1,15 +1,16 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.scss';
+import NavBar from './Components/Global/navBar'
 
 class App extends Component {
-  render() {
-    return (
-      <div>
-        Teste
-      </div>
-    );
-  }
+    render() {
+        return (
+            <div>
+                <NavBar></NavBar>
+            </div>
+        );
+    }
 }
 
 export default App;
---
+++ globart/src/Components/Global/navBar.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import ReactDOM from 'react-dom'
+
+
+class NavBar extends React.Component {
+	render() {
+		return (
+			<h1>Hello world</h1>
+			);
+	}
+}
+
+export default NavBar;
