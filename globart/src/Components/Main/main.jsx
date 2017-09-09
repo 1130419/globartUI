629a231a7fa8628c4067a413690d4cfb0b0397be
1130419 <1130419@isep.ipp.pt>
2017-09-09 15:56:23 +0100

    Correção em mobile

--- globart/src/Components/Main/main.jsx
+++ globart/src/Components/Main/main.jsx
@@ -79,18 +79,18 @@ class Main extends React.Component {
 							<h2>We specialize in mediation of pieces of art.</h2>
 						</header>
 						<div className="row features">
-							<section className="4u 12u(narrower) feature">
+							<section className="col-md-4 col-sm-12 feature">
 								<p>We are sure that you are a talented artist.
 									Here you can present your paintings to the world and who knows how to put
 									your work of art in a world-class museum.
 								</p>
 							</section>
-							<section className="4u 12u(narrower) feature">
+							<section className="col-md-4 col-sm-12 feature">
 								<p>Here you can increase your art collection or 
 									sell pieces of art that you have lost interest 
 									in and that someone may be willing to have.</p>
 							</section>
-							<section className="4u 12u(narrower) feature">
+							<section className="col-md-4 col-sm- 12feature">
 								<p>Would you like to buy a piece of art for your museum but 
 									you have no money at the moment? Here you can check if 
 									there is anyone willing to lend.
