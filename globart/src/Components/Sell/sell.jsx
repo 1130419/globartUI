6eba04cea7d264d837350bf872bcabf9f575e39f
José Cabeda <1130395@isep.ipp.pt>
2017-09-10 15:07:58 +0100

    Changed sell form

--- globart/src/Components/Sell/sell.jsx
+++ globart/src/Components/Sell/sell.jsx
@@ -1,15 +1,53 @@
 import React, { Component } from 'react';
 import 'bootstrap/dist/css/bootstrap.css';
+import DatePicker from 'react-datepicker';
+import moment from 'moment';
+
+import 'react-datepicker/dist/react-datepicker.css';
 
 class Sell extends Component {
+
+    constructor (props) {
+        super(props)
+        this.state = {
+            typeTransaction: "",
+            startDate: moment(),
+            endDate: moment(),
+            typeArt: "",
+        };
+        this.handleDateChange = this.handleDateChange.bind(this);
+        this.handleSelectionChange = this.handleSelectionChange.bind(this);
+        
+      }
+
+      submitForm()
+      {
+          alert("Submitted to API OP");
+      }
+    
+      handleDateChange(id, date) {
+        this.setState({
+          startDate: id === "startDate" ? date : this.state.starDate,
+          endDate: id === "endDate"? date :  this.state.endDate
+        });
+      }
+
+      handleSelectionChange(id, event) {
+        this.setState(
+            {
+                typeTransaction: id === "typeTransaction" && event.target.value,
+                typeArt: id === "typeArt" && event.target.value
+            });
+      }
+
     render() {
         return (
             <div className="container">
                 <form>
                     <div className="form-group">
-                        <div class="form-group">
-                            <label for="exampleFormControlSelect1">Type of transaction</label>
-                            <select class="form-control" id="exampleFormControlSelect1">
+                        <div className="form-group">
+                            <label htmlFor="typeTransaction">Type of transaction</label>
+                            <select className="form-control" selected = {this.state.typeTransaction} onChange={(event) => this.handleSelectionChange("typeTransaction", event)} id="typeTransaction">
                                 <option></option>
                                 <option>Sell</option>
                                 <option>Loan</option>
@@ -17,15 +55,21 @@ class Sell extends Component {
                         </div>
                     </div>
                     <div className="form-group">
-                        <label for="startDate">Start Date of transaction</label>
-                        <input type="date" classNameName="form-control" id="startDate" placeholder="Start Date"></input>
+                        <label htmlFor="startDate">Start Date of transaction</label>
+                        <DatePicker
+                            selected={this.state.startDate}
+                            onChange={(date) => this.handleDateChange("startDate", date)}
+                        />
                     </div>
                     <div className="form-group">
-                        <label for="endDate">End Date of transaction</label>
-                        <input type="date" className="form-control" id="endDate" placeholder="End Date"></input>
+                        <label htmlFor="endDate">End Date of transaction</label>
+                        <DatePicker
+                            selected={this.state.endDate}
+                            onChange={(date) => this.handleDateChange("endDate", date)}
+                        />
                     </div>
                     <div className="form-check">
-                        <label for="price">Price</label>
+                        <label htmlFor="price">Price</label>
                         <div className="input-group">
                             <input type="number" id="price" className="form-control" aria-label="Amount (to the nearest dollar)"></input>
                             <span className="input-group-addon">€</span>
@@ -33,42 +77,48 @@ class Sell extends Component {
                     </div>
                     <h1>Details of Piece of Art</h1>
                     <div className="form-group">
-                        <label for="nameArt">Name of Piece of Art</label>
+                        <label htmlFor="nameArt">Name of Piece of Art</label>
                         <input type="text" className="form-control" id="nameArt" placeholder="Name of Piece of Art"></input>
                     </div>
                     <div className="form-group">
-                        <label for="typeArt">Type of Piece of Art</label>
-                        <input type="text" className="form-control" id="typeArt" placeholder="Type of Piece of Art"></input>
-                    </div>
                     <div className="form-group">
-                        <label for="imageArt">Main Image</label>
+                        <label htmlFor="typeArt">Type of transaction</label>
+                        <select className="form-control" selected = {this.state.typeTransaction} onChange={(event) => this.handleSelectionChange("typeArt", event)} id="typeTransaction">
+                            <option></option>
+                            <option>Statue</option>
+                            <option>Paint</option>
+                        </select>
+                    </div>
+                </div>
+                    <div className="form-group">
+                        <label htmlFor="imageArt">Main Image</label>
                         <input type="fileUpload" className="form-control" id="imageArt" placeholder="Main Image"></input>
                     </div>
                     <div className="form-group">
-                        <label for="descrArt">Description</label>
+                        <label htmlFor="descrArt">Description</label>
                         <textarea className="form-control" id="descrArt" rows="3"></textarea>
                     </div>
                     <div className="form-group">
-                        <label for="authorArt">Author</label>
+                        <label htmlFor="authorArt">Author</label>
                         <input type="text" className="form-control" id="authorArt" placeholder="Author"></input>
                     </div>
                     <div className="form-group">
-                        <label for="yearArt">Year</label>
+                        <label htmlFor="yearArt">Year</label>
                         <input type="number" className="form-control" id="yearArt" placeholder="Year"></input>
                     </div>
                     <div className="form-group">
-                        <label for="periodArt">Period</label>
+                        <label htmlFor="periodArt">Period</label>
                         <input type="text" className="form-control" id="periodArt" placeholder="Period"></input>
                     </div>
                     <div className="form-group">
-                        <label for="dimArt">Dimensions</label>
+                        <label htmlFor="dimArt">Dimensions</label>
                         <input type="text" className="form-control" id="dimArt" placeholder="Year"></input>
                     </div>
                     <div className="form-group">
-                        <label for="estimatedPriceArt">Estimated Price</label>
+                        <label htmlFor="estimatedPriceArt">Estimated Price</label>
                         <input type="text" className="form-control" id="estimatedPriceArt" placeholder="Estimated Price"></input>
                     </div>
-                    <button type="submit" className="btn btn-primary">Submit</button>
+                    <button type="button" onClick={() => this.submitForm()} className="btn btn-primary pull-right">Submit</button>
                 </form>
             </div>
         );
